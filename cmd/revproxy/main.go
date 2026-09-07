aa4c580f44f08c8bbcfb722484b63222742a509d
Neffi42 <me@neffi.fr>
2026-09-07 19:17:50 +0200

    feat: add path-based routing to multiple upstreams

--- cmd/revproxy/main.go
+++ cmd/revproxy/main.go
@@ -5,6 +5,7 @@ import (
 	"log/slog"
 	"net/http"
 	"os"
+	"strings"
 	"time"
 
 	"github.com/Neffi42/my-http-reverse-proxy/internal/cache"
@@ -13,23 +14,30 @@ import (
 
 func main() {
 	listen := flag.String("listen", ":8000", "address to listen on")
-	upstream := flag.String("upstream", "http://127.0.0.1:9000", "address upstream is listening on")
 	flag.Parse()
 
-	logger := slog.Default()
-
-	rp, err := revproxy.New(*upstream, nil, logger)
-	if err != nil {
-		logger.Error("building reverse proxy", "err", err)
-		os.Exit(1)
+	routes := map[string]string{
+		"/api/":  "http://127.0.0.1:9001",
+		"/site/": "http://127.0.0.1:9002",
 	}
 
-	cacheStore := cache.NewStore()
-	cacheMiddleware := cache.New(rp, cacheStore, 5*time.Second, logger)
+	logger := slog.Default()
+	store := cache.NewStore()
 
-	logger.Info("proxy listening", "addr", *listen, "upstream", *upstream)
+	mux := http.NewServeMux()
+	for prefix, upstream := range routes {
+		rp, err := revproxy.New(upstream, nil, logger)
+		if err != nil {
+			logger.Error("building reverse proxy", "prefix", prefix, "err", err)
+			os.Exit(1)
+		}
+		strippedPreffix := http.StripPrefix(strings.TrimSuffix(prefix, "/"), rp)
+		cacheMiddleware := cache.New(strippedPreffix, store, 5*time.Second, logger)
+		mux.Handle(prefix, cacheMiddleware)
+	}
 
-	if err := http.ListenAndServe(*listen, cacheMiddleware); err != nil {
+	logger.Info("revproxy listening", "addr", *listen)
+	if err := http.ListenAndServe(*listen, mux); err != nil {
 		logger.Error("server stopped", "err", err)
 		os.Exit(1)
 	}
